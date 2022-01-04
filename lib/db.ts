61869888f37039b129347cd2e93df2d766f36ed4
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-04 14:18:33 -0500

    add ssl exception

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,13 @@
 import { Client } from 'pg'
 
-const client = new Client()
+const client = new Client({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT as any,
+  database: process.env.PGDATABASE,
+
+  ssl: { rejectUnauthorized: false },
+})
 
 export default client
