b857b3cc504dc2654cbc1a8e48b61d0468311722
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-04 18:41:56 -0500

    allow top level await

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack: (config) => {
+    config.experiments = { layers: true, topLevelAwait: true }
+    return config
+  },
   reactStrictMode: true,
 }
