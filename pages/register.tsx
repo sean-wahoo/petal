b9edbc041afd8c1bbd0922b6c0d69d8d9bc59d43
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-04 03:38:18 -0500

    removed useless imports and trimmed a bit

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,42 +1,32 @@
 import type { NextPage } from 'next'
-import Image from 'next/image'
 import styles from 'styles/layouts/register.module.scss'
-import splash from 'public/layered-waves-haikei.svg'
 
 const Register: NextPage = () => {
   return (
-    <>
-      <main className={styles.register}>
-        <h1>Register to ConnectHigh</h1>
-        <form className={styles.register__inputArea}>
-          <div className={styles.register__inputGroup}>
-            <label htmlFor='email'>Email</label>
-            <input
-              type='email'
-              id='email'
-              className={styles.register__emailInput}
-            />
-          </div>
-          <div className={styles.register__inputGroup}>
-            <label htmlFor='password'>Password</label>
-            <input
-              type='password'
-              id='password'
-              className={styles.register__passwordInput}
-            />
-          </div>
-          <button className={styles.register__registerButton} type='submit'>
-            Register
-          </button>
-        </form>
-        <Image
-          className={styles.register__splash}
-          src={splash}
-          alt='blob'
-          layout='responsive'
-        />
-      </main>
-    </>
+    <main className={styles.register}>
+      <h1>Register to ConnectHigh</h1>
+      <form className={styles.register__inputArea}>
+        <div className={styles.register__inputGroup}>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            id='email'
+            className={styles.register__emailInput}
+          />
+        </div>
+        <div className={styles.register__inputGroup}>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='password'
+            className={styles.register__passwordInput}
+          />
+        </div>
+        <button className={styles.register__registerButton} type='submit'>
+          Register
+        </button>
+      </form>
+    </main>
   )
 }
 
