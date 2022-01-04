daecc3be55cd62ca06b53d52eef2800997ad0855
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-04 04:16:44 -0500

    add controlled inputs to both pages and show password to register

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,14 +8,30 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 const Login: NextPage = () => {
   const [visible, setVisible] = useState<boolean>(false)
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
 
   return (
     <main className={styles.login}>
       <h1>Login to ConnectHigh</h1>
-      <form className={styles.login__inputArea}>
+      <form
+        className={styles.login__inputArea}
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault()
+          console.log({ email, password })
+        }}
+      >
         <div className={styles.login__inputGroup}>
           <label htmlFor='email'>Email</label>
-          <input type='email' id='email' className={styles.login__emailInput} />
+          <input
+            type='email'
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEmail(e.currentTarget.value)
+            }}
+            id='email'
+            className={styles.login__emailInput}
+          />
         </div>
         <div className={styles.login__inputGroup}>
           <label htmlFor='password'>Password</label>
@@ -23,6 +39,10 @@ const Login: NextPage = () => {
             <input
               type={visible ? 'text' : 'password'}
               id='password'
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setPassword(e.currentTarget.value)
+              }}
               className={styles.login__passwordInput}
             />
             <FontAwesomeIcon
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,26 +1,55 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import styles from 'styles/layouts/register.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 const Register: NextPage = () => {
+  const [visible, setVisible] = useState<boolean>(false)
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
   return (
     <main className={styles.register}>
       <h1>Register to ConnectHigh</h1>
-      <form className={styles.register__inputArea}>
+      <form
+        className={styles.register__inputArea}
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault()
+          console.log({ email, password })
+        }}
+      >
         <div className={styles.register__inputGroup}>
           <label htmlFor='email'>Email</label>
           <input
             type='email'
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEmail(e.currentTarget.value)
+            }}
             id='email'
             className={styles.register__emailInput}
           />
         </div>
         <div className={styles.register__inputGroup}>
           <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            id='password'
-            className={styles.register__passwordInput}
-          />
+          <div>
+            <input
+              type={visible ? 'text' : 'password'}
+              id='password'
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setPassword(e.currentTarget.value)
+              }}
+              className={styles.register__passwordInput}
+            />
+
+            <FontAwesomeIcon
+              icon={visible ? faEyeSlash : faEye}
+              className={styles.register__showPasswordIcon}
+              onClick={() => setVisible(!visible)}
+            />
+          </div>
         </div>
         <button className={styles.register__registerButton} type='submit'>
           Register
