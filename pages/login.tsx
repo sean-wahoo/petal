6c193c0054402de082f0af70ec002cfce2bfa389
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-04 03:25:02 -0500

    basic login/register page data

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,43 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import styles from 'styles/layouts/login.module.scss'
+import splash from 'public/layered-waves-haikei.svg'
 
 const Login: NextPage = () => {
-  return <div>login</div>
+  return (
+    <>
+      <main className={styles.login}>
+        <h1>Login to ConnectHigh</h1>
+        <form className={styles.login__inputArea}>
+          <div className={styles.login__inputGroup}>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              className={styles.login__emailInput}
+            />
+          </div>
+          <div className={styles.login__inputGroup}>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              id='password'
+              className={styles.login__passwordInput}
+            />
+          </div>
+          <button className={styles.login__loginButton} type='submit'>
+            Login
+          </button>
+        </form>
+        <Image
+          className={styles.login__splash}
+          src={splash}
+          alt='blob'
+          layout='responsive'
+        />
+      </main>
+    </>
+  )
 }
 
 export default Login
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,8 +1,43 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import styles from 'styles/layouts/register.module.scss'
+import splash from 'public/layered-waves-haikei.svg'
 
 const Register: NextPage = () => {
-  return <div>register</div>
+  return (
+    <>
+      <main className={styles.register}>
+        <h1>Register to ConnectHigh</h1>
+        <form className={styles.register__inputArea}>
+          <div className={styles.register__inputGroup}>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              className={styles.register__emailInput}
+            />
+          </div>
+          <div className={styles.register__inputGroup}>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              id='password'
+              className={styles.register__passwordInput}
+            />
+          </div>
+          <button className={styles.register__registerButton} type='submit'>
+            Register
+          </button>
+        </form>
+        <Image
+          className={styles.register__splash}
+          src={splash}
+          alt='blob'
+          layout='responsive'
+        />
+      </main>
+    </>
+  )
 }
 
 export default Register
