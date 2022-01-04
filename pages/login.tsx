30a7fbed7e1933c586f5f0c1479d2b885f133348
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-04 04:01:19 -0500

    add show password icon

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import styles from 'styles/layouts/login.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+
+// TODO: add other login providers
 
 const Login: NextPage = () => {
+  const [visible, setVisible] = useState<boolean>(false)
+
   return (
     <main className={styles.login}>
       <h1>Login to ConnectHigh</h1>
@@ -12,11 +19,18 @@ const Login: NextPage = () => {
         </div>
         <div className={styles.login__inputGroup}>
           <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            id='password'
-            className={styles.login__passwordInput}
-          />
+          <div>
+            <input
+              type={visible ? 'text' : 'password'}
+              id='password'
+              className={styles.login__passwordInput}
+            />
+            <FontAwesomeIcon
+              icon={visible ? faEyeSlash : faEye}
+              className={styles.login__showPasswordIcon}
+              onClick={() => setVisible(!visible)}
+            />
+          </div>
         </div>
         <button className={styles.login__loginButton} type='submit'>
           Login
