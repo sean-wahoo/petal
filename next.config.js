2a2c450eed62dad8b082166b05bdcb1d88040602
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-05 02:13:27 -0500

    redis was acting silly when it thought it was in the server so i had to do this

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.experiments = { layers: true, topLevelAwait: true }
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+      config.resolve.fallback.dns = false
+      config.resolve.fallback.net = false
+      config.resolve.fallback.tls = false
+    }
     return config
   },
   reactStrictMode: true,
