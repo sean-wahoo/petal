1901ad655ff4c77e79a6a77546c5c58e5ff47b91
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-05 02:12:46 -0500

    add functions to update the user session in redis and return it for client

---
+++ lib/session.ts
@@ -0,0 +1,45 @@
+import Redis from 'ioredis'
+import { nanoid } from 'nanoid/async'
+
+interface UserSessionData {
+  user_id: string
+  email: string
+}
+
+const updateSession = async (
+  user_id: string,
+  email: string
+): Promise<string> => {
+  const redis = new Redis(process.env.REDISURL)
+  const session_id = await nanoid(24)
+  redis.hmset(session_id, { user_id, email })
+  redis.expire(session_id, 84600)
+  return session_id
+}
+
+const getSession = async (session_id: string): Promise<UserSessionData> => {
+  let keys = await fetch(`${process.env.REDISAPIURL}/hkeys/${session_id}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.REDISTOKEN}`,
+    },
+  })
+  let vals = await fetch(`${process.env.REDISAPIURL}/hvals/${session_id}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.REDISTOKEN}`,
+    },
+  })
+
+  const keysParsed = await keys.json()
+  const valsParsed = await vals.json()
+
+  let arr = []
+  for (let i = 0; i < keysParsed.result.length; i++) {
+    arr.push([keysParsed.result[i], valsParsed.result[i]])
+  }
+
+  const obj: UserSessionData = Object.fromEntries(arr)
+
+  return obj
+}
+
+export { updateSession, getSession }
