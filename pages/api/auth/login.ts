7dc1931e84c4ce08ce05ca651845add5a136fe68
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-05 02:14:35 -0500

    removed unnecessary import

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import type { RowDataPacket } from 'mysql2'
 import connection from 'lib/db'
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
   interface LoginData {
