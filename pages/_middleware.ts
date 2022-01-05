c23c762fa8c3dce7fae0246015bee1aacd0ac613
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-05 02:13:11 -0500

    made the middleware do some redirecting and getting the user session from redis

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,7 @@
-import { NextResponse, NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
+import { getSession } from 'lib/session'
 
-export default function middleware() {
-  console.log('epic')
+export default async function middleware(req: NextRequest) {
+  if (!req.cookies.session_id) NextResponse.redirect('/login')
+  getSession(req.cookies.session_id)
 }
