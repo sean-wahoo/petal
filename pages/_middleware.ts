f93e41eb7c9ec45fd6ced8e1fe3e37e6c0a6bd98
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-05 22:31:19 -0500

    add correct redirecting

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getSession } from 'lib/session'
 
+interface SessionData {
+  user_id: string
+  email: string
+  session_id: string
+}
+
 export default async function middleware(req: NextRequest) {
-  if (!req.cookies.session_id) NextResponse.redirect('/login')
-  getSession(req.cookies.session_id)
+  if (!req.cookies.session_id) return NextResponse.redirect('/login')
+  const session_data: SessionData = await getSession(req.cookies.session_id)
+
+  console.log(session_data)
+  const { pathname } = req.nextUrl
+  if (pathname === '/register' || pathname === '/login')
+    return NextResponse.redirect('/')
 }
