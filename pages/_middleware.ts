0abe4b649431a2c410db6bdef1a1f740f446ae1d
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-06 00:59:56 -0500

    fix some redirect becasue i was wrong about rewrites and allow api calls

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -12,12 +12,17 @@ export default async function middleware(req: NextRequest) {
   const ext: string = pathname.split('.')[1]
   if (['svg', 'ico'].includes(ext)) return NextResponse.next()
 
-  if (!req.cookies.session_id && !['/login', '/register'].includes(pathname))
-    return NextResponse.rewrite('/login')
+  if (pathname.split('/')[1] === 'api') return NextResponse.next()
+
+  if (!req.cookies.session_id) {
+    if (['/login', '/register'].includes(pathname)) {
+      return NextResponse.next()
+    } else return NextResponse.redirect('login')
+  }
   const session_data: SessionData = await getSession(req.cookies.session_id)
   req.cookies.session_id?.length > 0 ||
     new NextResponse().cookie('session_id', session_data.session_id)
 
   if (pathname === '/register' || pathname === '/login')
-    return NextResponse.rewrite('/')
+    return NextResponse.redirect('/')
 }
