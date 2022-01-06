6eff6fc256f70dd91085fa3abfba6cb8fce4eee7
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-05 23:15:49 -0500

    fix redirect and dont use the redirect function

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -7,13 +7,17 @@ interface SessionData {
   session_id: string
 }
 
-export default async function middleware(req: NextRequest, res: NextResponse) {
-  if (!req.cookies.session_id) return NextResponse.redirect('/login')
-  const session_data: SessionData = await getSession(req.cookies.session_id)
-  req.cookies.session_id.length > 0 ||
-    res.cookie('session_id', session_data.session_id)
-
+export default async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
+  const ext: string = pathname.split('.')[1]
+  if (['svg', 'ico'].includes(ext)) return NextResponse.next()
+
+  if (!req.cookies.session_id && !['/login', '/register'].includes(pathname))
+    return NextResponse.rewrite('/login')
+  const session_data: SessionData = await getSession(req.cookies.session_id)
+  req.cookies.session_id?.length > 0 ||
+    new NextResponse().cookie('session_id', session_data.session_id)
+
   if (pathname === '/register' || pathname === '/login')
-    return NextResponse.redirect('/')
+    return NextResponse.rewrite('/')
 }
