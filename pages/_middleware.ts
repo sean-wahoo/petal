f77eb6a34a49278d51af3e4e2d6223acf3a95c50
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-06 02:37:55 -0500

    handle redirects and sessions better

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,10 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getSession } from 'lib/session'
+import destroySession from './api/auth/destroySession'
 
 interface SessionData {
   user_id: string
   email: string
   session_id: string
+  isError: boolean
+}
+
+interface SessionErrorData {
+  isError: boolean
+  message: string
 }
 
 export default async function middleware(req: NextRequest) {
@@ -19,9 +26,33 @@ export default async function middleware(req: NextRequest) {
       return NextResponse.next()
     } else return NextResponse.redirect('login')
   }
-  const session_data: SessionData = await getSession(req.cookies.session_id)
-  req.cookies.session_id?.length > 0 ||
+  const session_data: SessionData | SessionErrorData = await getSession(
+    req.cookies.session_id
+  )
+
+  console.log(session_data)
+  if (
+    session_data.isError &&
+    'message' in session_data &&
+    session_data.message === 'Invalid Session ID!'
+  ) {
+    const session_id = req.cookies.session_id
+    const dev = process.env.NODE_ENV !== 'production'
+
+    await fetch(
+      `${dev ? 'http://localhost:3000' : null}/api/auth/destroySession`,
+      {
+        method: 'POST',
+        body: JSON.stringify({ session_id }),
+      }
+    )
+    const res = NextResponse.redirect('/login')
+    return res.clearCookie('session_id')
+  }
+
+  if (req.cookies.session_id?.length === 0 && 'session_id' in session_data) {
     new NextResponse().cookie('session_id', session_data.session_id)
+  }
 
   if (pathname === '/register' || pathname === '/login')
     return NextResponse.redirect('/')
