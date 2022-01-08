796c76d1f605220428d387c048e4c71f1909c1be
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-08 00:02:51 -0500

    add simple error message

---
+++ components/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+import { ErrorMessageProps } from 'lib/types'
+import React from 'react'
+import styles from 'styles/components/error_message.module.scss'
+
+export default function ErrorMessage(props: ErrorMessageProps) {
+  const { error_message } = props
+  return (
+    <div className={styles.error_container}>
+      <h5 className={styles.error_message}>{error_message}</h5>
+    </div>
+  )
+}
