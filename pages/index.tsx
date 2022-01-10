36865d8a71180cd1b8ad04d975194180e834b1f7
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-09 21:19:22 -0500

    empty index styles and stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,30 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetServerSideProps } from 'next'
+import type { SessionProps, SessionError, SessionSuccess } from 'lib/types'
 import styles from 'styles/layouts/index.module.scss'
+import { logout } from 'lib/utils'
+import { session_handler } from 'lib/session'
+import Router from 'next/router'
 
-const Index: NextPage = () => {
-  return <div>index</div>
+const Index: NextPage<SessionProps> = ({ session }) => {
+  return <main className={styles.index}></main>
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  let session: SessionSuccess | SessionError | Object = {}
+  try {
+    session = await session_handler(context.req.cookies.session_id)
+    return {
+      props: { session },
+    }
+  } catch (e: any) {
+    context.res.setHeader('Set-Cookie', ['session_id=deleted; Max-Age=0'])
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
 }
 
 export default Index
