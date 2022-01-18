7529bf3baad26906eb64a8970ca0bafa1f063374
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-17 21:19:44 -0500

    allow layout to pass profile props and help styling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { LayoutProps } from 'lib/types'
 import Head from 'next/head'
+import Navbar from 'components/Navbar'
+import styles from 'styles/components/layout.module.scss'
 
-export default function Layout({ children, title }: LayoutProps) {
+export default function Layout({
+  children,
+  title,
+  is_auth,
+  session_data,
+}: LayoutProps) {
   return (
-    <>
+    <div className={styles.layout}>
       <Head>
         <title>{title}</title>
       </Head>
+      {is_auth && <Navbar profile={{ ...session_data }} />}
       {children}
-    </>
+    </div>
   )
 }
