c412cc0f9ed61babd3d8309da24d1f991a25dfe4
Sean Reichel <sean.p.reichel@gmail.com>
2022-05-09 23:39:19 -0400

    added nice dropdown menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,12 @@ import debounce from "lodash.debounce";
 import Image from "next/image";
 import Link from "next/link";
 import type { ProfileProps } from "lib/types";
-import { upload } from "lib/bucket";
-import { updateSession } from "lib/session";
+
 export default function Navbar({ profile }: ProfileProps) {
   const [prev, setPrev] = useState<number>(0);
-  const navRef = useRef(null);
-  console.log(profile);
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const navRef = useRef<HTMLElement>(null);
   useEffect(() => {
     window.addEventListener(
       "scroll",
@@ -28,19 +28,44 @@ export default function Navbar({ profile }: ProfileProps) {
     );
   }, [prev]);
 
-  console.log({ profile });
+  const handleClickOutside = (e: MouseEvent) => {
+    console.log("click!", dropdownRef.current, showDropdown);
+    if (
+      !dropdownRef.current?.contains(e.target as any) &&
+      !document.getElementById("profile-image")?.contains(e.target as any)
+    ) {
+      setShowDropdown(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () =>
+      document.removeEventListener("click", handleClickOutside, true);
+  }, [showDropdown]);
 
   return (
-    <nav className={`${styles.navbar}`} ref={navRef}>
+    <nav className={styles.navbar} ref={navRef}>
       <Link href="/">ConnectHigh</Link>
       <Image
         src={profile.image_url}
         alt={profile.display_name}
         width={48}
         height={48}
+        id="profile-image"
         className={styles.profile_image}
+        onClick={(e: any) => {
+          setShowDropdown(!showDropdown);
+        }}
       />
-      <form>
+      {showDropdown && (
+        <div ref={dropdownRef} className={styles.dropdown} id="dropdown">
+          <ul>
+            <li>Profile</li>
+            <li>Settings</li>
+          </ul>
+        </div>
+      )}
+      {/* <form>
         <input
           type="file"
           accept="image/*"
@@ -55,7 +80,6 @@ export default function Navbar({ profile }: ProfileProps) {
               console.log("whoops", { error });
               return;
             }
-            console.log({ data });
             updateSession({
               ...profile,
               email: data.email,
@@ -63,7 +87,7 @@ export default function Navbar({ profile }: ProfileProps) {
             });
           }}
         />
-      </form>
+      </form> */}
     </nav>
   );
 }
