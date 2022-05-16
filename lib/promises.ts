e450f13467910587b5ac2b4d45330980c0d8adba
Sean Reichel <sean.p.reichel@gmail.com>
2022-05-16 17:08:15 -0400

    added a resolver function to handle data better

--- lib/promises.ts
+++ lib/promises.ts
@@ -1,11 +1,13 @@
+import { AxiosResponse } from "axios";
+
 export const fetcher = (...args: any) =>
-  fetch.apply(null, args).then((res) => res.json())
+  fetch.apply(null, args).then((res) => res.json());
 
 export const resolver = async (promise: Promise<AxiosResponse<any, any>>) => {
   try {
-    let res = await Promise.resolve(promise)
-    return [res.data, null]
-  } catch (e) {
-    return [null, e.response.data]
+    let res = await Promise.resolve(promise);
+    return [res.data, null];
+  } catch (e: any) {
+    return [null, e.response.data];
   }
-}
+};
