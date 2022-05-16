49daba18d64e0bb7dcc581794835527f981989b2
Sean Reichel <sean.p.reichel@gmail.com>
2022-05-16 17:11:23 -0400

    update session helpers

--- lib/session.ts
+++ lib/session.ts
@@ -1,30 +1,35 @@
-import { nanoid } from 'nanoid/async'
-import type { SessionSuccess, SessionError, SessionData } from 'lib/types'
-import Cookies from 'universal-cookie'
+import { nanoid } from "nanoid/async";
+import type { SessionSuccess, SessionError, SessionData } from "lib/types";
+import Cookies from "universal-cookie";
+import axios from "axios";
 
-const updateSessionWithSessionId = async(session_id: string, session_addition: Object): Promise<Response | SessionError> => {
+const updateSessionWithSessionId = async (
+  session_id: string,
+  session_addition: Object
+): Promise<Response | SessionError> => {
   try {
-    let all = await fetch(`${process.env.NEXT_PUBLIC_REDIS_API_URL}/hgetall/${session_id}`, {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
-      },
-    })
-  
-    const allParsed = await all.json()
+    let all = await fetch(
+      `${process.env.NEXT_PUBLIC_REDIS_API_URL}/hgetall/${session_id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
+        },
+      }
+    );
+
+    const allParsed = await all.json();
 
     let data = await fetch(
       `${process.env.NEXT_PUBLIC_REDIS_API_URL}/hmset/${session_id}/session_data`,
       {
-        method: 'POST',
+        method: "POST",
         headers: {
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
-
         },
         body: JSON.stringify({ ...allParsed, ...session_addition }),
       }
-    )
-    data = await data.json()
-    console.log({ data })
+    );
+    data = await data.json();
 
     let exp = await fetch(
       `${process.env.NEXT_PUBLIC_REDIS_API_URL}/expire/${session_id}/84600`,
@@ -33,41 +38,37 @@ const updateSessionWithSessionId = async(session_id: string, session_addition: O
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
         },
       }
-    )
-    exp = await exp.json()
+    );
+    exp = await exp.json();
 
-    if ('error' in data || 'error' in exp)
-      throw new Error('Session failed to initialize!')
+    if ("error" in data || "error" in exp)
+      throw new Error("Session failed to initialize!");
 
     return exp;
   } catch (e: any) {
-    return { is_error: true, error_code: e.code, error_message: e.message }
+    return { is_error: true, error_code: e.code, error_message: e.message };
   }
-}
+};
 
 const updateSession = async (
   session_data: SessionData
 ): Promise<string | SessionError> => {
   try {
-    let { user_id, email, session_id, image_url, display_name } = session_data
-    session_id ||= await nanoid(24)
-    const obj: any = { user_id, email, image_url, display_name }
-
-    console.log({obj})
+    let { user_id, email, session_id, image_url, display_name } = session_data;
+    session_id ||= await nanoid(24);
+    const obj: any = { user_id, email, image_url, display_name };
 
     let data = await fetch(
       `${process.env.NEXT_PUBLIC_REDIS_API_URL}/hmset/${session_id}/session_data`,
       {
-        method: 'POST',
+        method: "POST",
         headers: {
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
-
         },
         body: JSON.stringify(obj),
       }
-    )
-    data = await data.json()
-    console.log({ data })
+    );
+    data = await data.json();
 
     let exp = await fetch(
       `${process.env.NEXT_PUBLIC_REDIS_API_URL}/expire/${session_id}/84600`,
@@ -76,86 +77,98 @@ const updateSession = async (
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
         },
       }
-    )
-    exp = await exp.json()
+    );
+    exp = await exp.json();
 
-    if ('error' in data || 'error' in exp)
-      throw new Error('Session failed to initialize!')
+    if ("error" in data || "error" in exp)
+      throw {
+        code: "session-failed",
+        message: "Session Failed to Initialize!",
+      };
 
-    return session_id
+    return session_id;
   } catch (e: any) {
-    return { is_error: true, error_code: e.code, error_message: e.message }
+    return { is_error: true, error_code: e.code, error_message: e.message };
   }
-}
+};
 
 const getSession = async (
   session_id: string
 ): Promise<SessionSuccess | SessionError> => {
-  let all = await fetch(`${process.env.NEXT_PUBLIC_REDIS_API_URL}/hgetall/${session_id}`, {
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
-    },
-  })
+  let all = await fetch(
+    `${process.env.NEXT_PUBLIC_REDIS_API_URL}/hgetall/${session_id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
+      },
+    }
+  );
 
-  const allParsed = await all.json()
+  const allParsed = await all.json();
 
   if (allParsed.result.length === 0)
     return {
       is_error: true,
-      error_code: '',
-      error_message: 'Invalid Session ID!',
-    }
+      error_code: "invalid-session-id",
+      error_message: "Invalid Session ID!",
+    };
 
   const obj = {
     ...JSON.parse(allParsed.result[1]),
     session_id,
     is_error: false,
-  }
+  };
 
-  return obj
-}
+  return obj;
+};
 
 const destroySession = async (session_id: string): Promise<void> => {
   try {
-    await fetch(`${process.env.NEXT_PUBLIC_REDIS_API_URL}/del/${session_id}`, {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
-      },
-      body: JSON.stringify({ session_id }),
-    })
+    await axios.post(
+      `${process.env.NEXT_PUBLIC_REDIS_API_URL}/del/${session_id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_REDIS_API_TOKEN}`,
+        },
+        body: JSON.stringify({ session_id }),
+      }
+    );
   } catch (e: any) {
-    console.log({ e })
+    console.log({ e });
   }
-}
+};
 
 const session_handler = async (
   session_id: string | undefined
 ): Promise<SessionSuccess | SessionError> => {
   if (session_id === undefined || session_id.length < 1)
-    throw new Error('No session cookie!')
+    throw { message: "No Session Cookie!", code: "no-session-cookie" };
 
   const session_data: SessionSuccess | SessionError = await getSession(
     session_id
-  )
+  );
 
   if (
-    'is_error' in session_data &&
-    session_data.error_message === 'Invalid Session ID!'
+    "is_error" in session_data &&
+    session_data.error_message === "Invalid Session ID!"
   ) {
-    const dev = process.env.NODE_ENV !== 'production'
+    const dev = process.env.NODE_ENV !== "production";
 
     await fetch(
-      `${dev ? 'http://localhost:3000' : null}/api/auth/destroySession`,
+      `${dev ? "http://localhost:3000" : null}/api/auth/destroySession`,
       {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify({ session_id }),
       }
-    )
-    const cookies = new Cookies()
-    cookies.remove('session_id')
-    throw new Error('Invalid Session ID!')
+    );
+    const cookies = new Cookies();
+    cookies.remove("session_id");
+    throw {
+      message: "Invalid Session ID!",
+      code: "invalid-session-id",
+    };
   }
 
-  return session_data
-}
-export { updateSession, getSession, destroySession, session_handler }
+  return session_data;
+};
+export { updateSession, getSession, destroySession, session_handler };
