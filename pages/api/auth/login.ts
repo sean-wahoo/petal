8019ae8a0dc1f30061bd1ff50e3a358360483364
Sean Reichel <sean.p.reichel@gmail.com>
2022-05-16 17:13:41 -0400

    add prisma

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,36 +1,35 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type { AuthData, LoginResponse } from 'lib/types'
-import { updateSession } from 'lib/session'
-import bcrypt from 'bcrypt'
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { AuthData, LoginResponse } from "lib/types";
+import { updateSession } from "lib/session";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
   try {
-    
-    const { email, password }: AuthData = req.body
+    const { email, password }: AuthData = req.body;
 
     if (email.length === 0 || password.length === 0) {
-      throw { message: 'Please provide a email and password!' }
+      throw { message: "Please provide a email and password!" };
     }
 
     const emailPattern =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!email.toLowerCase().match(emailPattern)) {
-      throw { message: 'Please provide a valid email address!', type: 'email' }
+      throw { message: "Please provide a valid email address!", type: "email" };
     }
     const numRowsWithEmail = await prisma.users.findMany({
       where: { email: email },
-    })
+    });
 
     if (numRowsWithEmail.length === 0) {
-      throw { message: 'No user with that email exists!', type: 'email' }
+      throw { message: "No user with that email exists!", type: "email" };
     }
 
     if (
       !(await bcrypt.compare(password, numRowsWithEmail[0].password as string))
     ) {
-      throw { message: 'That password is incorrect!', type: 'password' }
+      throw { message: "That password is incorrect!", type: "password" };
     }
 
     const session_data = {
@@ -38,23 +37,24 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
       email,
       display_name: numRowsWithEmail[0].display_name,
       image_url: numRowsWithEmail[0].image_url,
-    }
-    const session_id = await updateSession(session_data)
+    };
+    const session_id = await updateSession(session_data);
     prisma.users.update({
       where: { user_id: numRowsWithEmail[0].user_id },
       data: { session_id: session_id as string },
-    })
+    });
     return res.status(200).json({
       user_id: numRowsWithEmail[0].user_id,
       session_id,
-    } as LoginResponse)
+    } as LoginResponse);
   } catch (e: any) {
+    console.log({ e });
     res.status(500).json({
       is_error: true,
       error_code: e.code,
       error_message: e.message,
       type: e.type,
-    } as LoginResponse)
+    } as LoginResponse);
   } finally {
     prisma.$disconnect();
   }
--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,50 +1,51 @@
-import faker from '@faker-js/faker'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type { AuthData, RegisterResponse } from 'lib/types'
-import { updateSession } from 'lib/session'
-import bcrypt from 'bcrypt'
-import { nanoid } from 'nanoid/async'
-import { PrismaClient } from '@prisma/client'
+import faker from "@faker-js/faker";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { AuthData, RegisterResponse } from "lib/types";
+import { updateSession } from "lib/session";
+import bcrypt from "bcrypt";
+import { nanoid } from "nanoid/async";
+import { PrismaClient } from "@prisma/client";
 
 export default async function register(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const prisma = new PrismaClient();
+
   try {
-    const prisma = new PrismaClient()
-    const { email, password }: AuthData = req.body
+    const { email, password }: AuthData = req.body;
 
     if (email.length === 0 || password.length === 0) {
-      throw { message: 'Please provide an email and password!' }
+      throw { message: "Please provide an email and password!" };
     }
 
     const emailPattern =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (!email.toLowerCase().match(emailPattern)) {
-      throw { message: 'Please provide a valid email address!', type: 'email' }
+      throw { message: "Please provide a valid email address!", type: "email" };
     }
 
     const numRowsWithEmail = await prisma.users.findMany({
       where: { email: email },
-    })
+    });
 
     if (numRowsWithEmail.length > 0) {
-      throw { message: 'That email is already in use!', type: 'email' }
+      throw { message: "That email is already in use!", type: "email" };
     }
 
-    const user_id = await nanoid(11)
-    const salt = await bcrypt.genSalt()
-    const hash = await bcrypt.hash(password, salt)
-    const seed = await nanoid(16)
-    const image_url = `https://avatars.dicebear.com/api/bottts/${seed}.svg`
-    const display_name = faker.internet.userName()
+    const user_id = await nanoid(11);
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+    const seed = await nanoid(16);
+    const image_url = `https://avatars.dicebear.com/api/bottts/${seed}.svg`;
+    const display_name = faker.internet.userName();
     const session_id = await updateSession({
       user_id,
       email,
       image_url,
       display_name,
-    })
+    });
 
     await prisma.users.create({
       data: {
@@ -55,15 +56,17 @@ export default async function register(
         image_url,
         display_name,
       },
-    })
+    });
 
-    return res.status(200).json({ user_id, session_id } as RegisterResponse)
+    return res.status(200).json({ user_id, session_id } as RegisterResponse);
   } catch (e: any) {
     res.status(500).json({
       is_error: true,
       error_code: e.code,
       error_message: e.message,
       type: e.type,
-    } as RegisterResponse)
+    } as RegisterResponse);
+  } finally {
+    prisma.$disconnect();
   }
 }
