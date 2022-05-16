0b3c1162cba5716aee5162360eb6fab614d696c3
Sean Reichel <sean.p.reichel@gmail.com>
2022-05-16 17:11:59 -0400

    update and add tons of types

--- lib/types.ts
+++ lib/types.ts
@@ -1,63 +1,63 @@
 interface Success {
-  user_id: string
-  session_id: string
+  user_id: string;
+  session_id: string;
 }
 
 interface Error {
-  is_error: boolean
-  error_code: string
-  error_message: string
+  is_error: boolean;
+  error_code: string;
+  error_message: string;
 }
 
 interface AuthData {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
 
 interface SessionData {
-  user_id: string
-  email: string
-  session_id?: string
-  display_name: string
-  image_url: string
+  user_id: string;
+  email: string;
+  session_id?: string;
+  display_name: string;
+  image_url: string;
 }
 
 interface SessionProps {
-  session: SessionData
+  session: SessionData;
 }
 
 interface RegisterSuccess extends Success {
-  type: string
+  type: string;
 }
 
 interface RegisterError extends Error {
-  type: string
+  type: string;
 }
 
 interface RegisterResponse extends RegisterSuccess, RegisterError {}
 
 interface LoginSuccess extends Success {
-  type: string
+  type: string;
 }
 
 interface LoginError extends Error {
-  type: string
+  type: string;
 }
 
 interface LoginResponse extends LoginSuccess, LoginError {}
 
 interface SessionSuccess extends Success, SessionData {
-  display_name: string
-  session_id: string
-  image_url: string
-  email: string
+  display_name: string;
+  session_id: string;
+  image_url: string;
+  email: string;
 }
 
 interface SessionError extends Error {}
 
 interface LogoutData {
-  user_id: string
-  session_id: string
+  user_id: string;
+  session_id: string;
 }
 
 interface LogoutSuccess extends Success {}
@@ -66,23 +66,42 @@ interface LogoutError extends Error {}
 
 // props
 interface ErrorMessageProps {
-  error_message: string
-  type?: string
+  error_message: string;
+  type?: string;
 }
 
 interface LayoutProps {
-  children: any
-  title: string
-  is_auth: boolean
-  session_data?: SessionData
+  children: any;
+  title: string;
+  is_auth: boolean;
+  session_data?: SessionData;
+  showNavbar?: boolean;
 }
 interface ProfileProps {
   profile: {
-    user_id: string
-    display_name: string
-    image_url: string
-  }
+    user_id: string;
+    display_name: string;
+    image_url: string;
+  };
 }
+
+interface ProfilePageProps extends SessionProps {
+  profile_data: {
+    user_id: string;
+    display_name: string;
+    email: string;
+    created_at: string;
+    updated_at: string;
+    image_url: string;
+    date_of_birth: string;
+    tagline: string;
+  };
+}
+
+interface ProfileDataSuccess extends Success {}
+interface ProfileDataError extends Error {}
+
+interface ProfileDataResponse extends ProfileDataSuccess, ProfileDataError {}
 
 export type {
   SessionProps,
@@ -100,4 +119,6 @@ export type {
   ErrorMessageProps,
   LayoutProps,
   ProfileProps,
-}
+  ProfilePageProps,
+  ProfileDataResponse,
+};
