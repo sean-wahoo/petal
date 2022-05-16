ea29a9c1bfb2d65b7d663034f18720e12fcd21da
Sean Reichel <sean.p.reichel@gmail.com>
2022-05-16 17:18:02 -0400

    add route to set welcomed status

---
+++ pages/api/users/welcome-user.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+const prisma = new PrismaClient();
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { user_id } = req.query;
+    await prisma.users.update({
+      where: {
+        user_id: user_id as string,
+      },
+      data: {
+        been_welcomed: true,
+      },
+    });
+    return res.status(200).json({ message: "success" });
+  } catch (e: any) {
+    console.log({ e });
+    res.status(500).json({
+      is_error: true,
+      error_code: e.code,
+      error_message: e.message,
+    });
+  } finally {
+    prisma.$disconnect();
+  }
+};
