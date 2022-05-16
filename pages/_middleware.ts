2ecbc1e596c8c7a4fe32ccfe75de6a73feeb57dc
Sean Reichel <sean.p.reichel@gmail.com>
2022-05-16 17:12:33 -0400

    improve middleware performance and functionality

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,27 +1,26 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl
-  const ext: string = pathname.split('.')[1]
-  if (['svg', 'ico'].includes(ext)) return NextResponse.next()
+  const { pathname } = req.nextUrl;
+  const ext: string = pathname.split(".")[1];
+  if (["svg", "ico"].includes(ext)) return NextResponse.next();
 
-  if (pathname.split('/')[1] === 'api') return NextResponse.next()
+  if (pathname.split("/")[1] === "api") return NextResponse.next();
 
   if (!req.cookies.session_id) {
-    if (['/login', '/register'].includes(pathname)) {
-    } else {
-      const url = req.nextUrl.clone()
-      url.pathname = '/login'
-      return NextResponse.redirect(url)
+    if (!["/login", "/register"].includes(pathname)) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
     }
-    return NextResponse.next()
+    return NextResponse.next();
+  }
+  if (pathname === "/register" || pathname === "/login") {
+    const url = req.nextUrl.clone();
+
+    url.pathname = "/welcome";
+    return NextResponse.redirect(url);
   }
 
-  if (pathname === '/register' || pathname === '/login') {
-    const url = req.nextUrl.clone()
-
-    url.pathname = '/'
-    return NextResponse.redirect(url)
-  }
-  return NextResponse.next()
+  return NextResponse.next();
 }
