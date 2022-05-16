df02e404e353d9aad6e1cc046cc81bfeee8dfbed
Sean Reichel <sean.p.reichel@gmail.com>
2022-05-16 17:12:41 -0400

    switch to prisma

--- pages/api/auth/destroySession.ts
+++ pages/api/auth/destroySession.ts
@@ -1,19 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
 export default async function destroySession(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const prisma = new PrismaClient();
   try {
-    const prisma = new PrismaClient()
-    const { session_id }: { [k: string]: string } = JSON.parse(req.body)
+    const { session_id }: { [k: string]: string } = JSON.parse(req.body);
     await prisma.users.updateMany({
       where: { session_id: session_id },
       data: { session_id: null },
-    })
-    return res.status(200).json({ message: 'yea' })
+    });
+    return res.status(200).json({ message: "yea" });
   } catch (e: any) {
-    console.log({ e })
+    console.log({ e });
+  } finally {
+    prisma.$disconnect();
   }
 }
