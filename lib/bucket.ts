ebe581d8565c0dac00ae60cb0308b36594a2f68e
Sean Reichel <sean.p.reichel@gmail.com>
2022-05-21 23:02:08 -0400

    i dont know what changed but ok

--- lib/bucket.ts
+++ lib/bucket.ts
@@ -1,27 +1,27 @@
-import * as AWS from 'aws-sdk'
-import axios from 'axios'
+import * as AWS from "aws-sdk";
+import axios from "axios";
 const bucket = new AWS.S3({
   accessKeyId: process.env.NEXT_PUBLIC_AWS_ID,
   secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET,
-})
+});
 export const upload = async (file: File, folder: string, user_id: string) => {
   try {
-    file = new File([file], `${user_id}.${file.name.split('.').at(-1)}`)
+    file = new File([file], `${user_id}.${file.name.split(".").at(-1)}`);
     const params = {
       Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME as string,
       Key: `${folder}/${file.name}`,
       Body: file,
-      ACL: 'public-read'
-    }
+      ACL: "public-read",
+    };
     const bucketData = await bucket.upload(params).promise();
     const image_url = bucketData.Location;
-    const { data } = await axios.patch('/api/auth/updateProfileImage', {
+    const { data } = await axios.patch("/api/auth/updateProfileImage", {
       user_id,
       image_url,
-    })
-    return [{ email: data.email, user_id: data.user_id, image_url }, null]
+    });
+    return [{ email: data.email, user_id: data.user_id, image_url }, null];
   } catch (e: any) {
-    console.log({ e })
-    return [null, e]
+    console.error({ e });
+    return [null, e];
   }
-}
+};
