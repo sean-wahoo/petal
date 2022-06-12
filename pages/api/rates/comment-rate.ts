1c08157fd426925a4671846989b2dfb5361e017d
Sean Reichel <sean.p.reichel@gmail.com>
2022-06-11 20:29:27 -0400

    add rate routes

---
+++ pages/api/rates/comment-rate.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function commentRate(
+  req: NextApiRequest, res: NextApiResponse,
+) {
+  const prisma = new PrismaClient();
+  try {
+    const { rate_kind, user_rate_id, comment_rate_id, remove_rate } = req.body
+
+    console.log({ rate_kind, user_rate_id, comment_rate_id, remove_rate })
+
+    if (remove_rate) {
+      const data = await prisma.comment_rates.delete({
+        where: {
+          user_rate_id_comment_rate_id: {
+            user_rate_id, comment_rate_id
+          }
+        }
+      })
+      return res.status(200).json({ message: 'success-rate-delete', data })
+    }
+    const data = await prisma.comment_rates.upsert({
+      create: {
+        rate_kind, user_rate_id, comment_rate_id
+      },
+      update: {
+        rate_kind
+      },
+      where: {
+        user_rate_id_comment_rate_id: {
+          user_rate_id, comment_rate_id
+        }
+      }
+    })
+
+    return res.status(200).json({ message: 'success-rate-upsert', data })
+
+  } catch (e: any) {
+    res.status(500).json({
+      is_error: true,
+      error_code: e.code,
+      error_message: e.message,
+    });
+  }
+  finally {
+    prisma.$disconnect()
+  }
+}
+
