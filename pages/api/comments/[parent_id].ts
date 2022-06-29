b9d6bebce6c9256b5e6c1b382d1d948bda204ce9
Sean Reichel <sean.p.reichel@gmail.com>
2022-06-28 22:48:13 -0400

    remove import

--- pages/api/comments/[parent_id].ts
+++ pages/api/comments/[parent_id].ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function getComments(
