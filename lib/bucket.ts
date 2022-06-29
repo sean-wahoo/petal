f97223a7ab5f5dfcf5efd719f9cdbd3824541cf7
Sean Reichel <sean.p.reichel@gmail.com>
2022-06-28 22:45:05 -0400

    rework uploading profile images

--- lib/bucket.ts
+++ lib/bucket.ts
@@ -1,66 +1,9 @@
 import * as AWS from "aws-sdk";
-import { PassThrough, Readable } from "stream";
-import ffmpeg from "fluent-ffmpeg"
-import fs from "fs"
-import sharp from 'sharp'
 
 const bucket = new AWS.S3({
   accessKeyId: process.env.NEXT_PUBLIC_AWS_ID,
   secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET,
 });
-
-export const newConvertImage = (readable: Readable) => {
-  const passthrough = new PassThrough()
-  ffmpeg()
-    .input(readable)
-    .outputFormat('webp')
-    .stream(passthrough, { end: true })
-  return passthrough;
-}
-
-export const convertImage = async (image: Readable, outputFormat: string) => {
-  console.log({ image })
-  const p = new Promise((resolve, reject) => {
-    const chunks: Buffer[] = []
-    const passThrough = new PassThrough()
-    const newImage = ffmpeg()
-      .input(image)
-      .outputFormat(outputFormat)
-      // .on('start', () => console.log('start'))
-      // .on('progress', () => console.log('progress...'))
-      // .on('error', (e) => console.log(e))
-      // .pipe(res, { end: true })
-      // .on('error', reject)
-      .stream(passThrough, { end: true })
-    passThrough.on('data', data => {
-      chunks.push(data)
-      console.log({ data })
-    })
-    passThrough.on('error', reject)
-    passThrough.on('end', () => {
-      const originalImage = Buffer.concat(chunks);
-      const editedImage = originalImage
-      // .copyWithin(4, -4)
-      // .slice(0, -4)
-      return resolve(editedImage)
-    })
-    resolve(newImage)
-  })
-  p.then(editedImage => {
-    console.log({ editedImage })
-  }).catch(e => {
-    console.log({ e })
-  })
-}
-
-export const bufferToStream = (buffer: Buffer): Readable => {
-  const readable = new Readable();
-  readable._read = () => { }
-  readable.push(buffer)
-  readable.push(null)
-  console.log({ readable })
-  return readable
-}
 
 export const upload = async (file: Buffer, folder: string, user_id: string) => {
   try {
@@ -72,8 +15,8 @@ export const upload = async (file: Buffer, folder: string, user_id: string) => {
       ContentType: 'image/webp'
     };
     const bucketData = await bucket.upload(params).promise();
-    const image_url = bucketData.Location;
-    return [{ user_id, image_url }]
+    const image_url: string = bucketData.Location;
+    return [{ user_id, image_url }, null]
   } catch (e: any) {
     console.error({ e });
     return [null, e];
--- pages/users/[user_id].tsx
+++ pages/users/[user_id].tsx
@@ -1,7 +1,7 @@
 import Layout from "components/Layout";
 import { NextPage, GetStaticPaths, GetStaticProps } from "next";
 import styles from "styles/layouts/profile.module.scss";
-import type { ProfilePageProps, SessionData } from "lib/types";
+import type { ProfilePageProps } from "lib/types";
 import { getApiUrl } from "lib/utils";
 import axios from "axios";
 import Image from "next/image";
@@ -11,6 +11,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import Dropdown from "components/Dropdown";
 import { resolver } from "lib/promises";
+import { upload } from "lib/bucket";
+import { revalidate } from "lib/utils";
+import { PrismaClient } from "@prisma/client";
 
 const ProfilePage: NextPage<ProfilePageProps> = ({ profile_data, friends_data }) => {
   const [friendStatus, setFriendStatus] = useState<string | null>(null);
@@ -19,7 +22,6 @@ const ProfilePage: NextPage<ProfilePageProps> = ({ profile_data, friends_data })
   const toggleRef = useRef<HTMLSpanElement>(null)
   const { session, updateSession } = useSession();
   const imageUploadInputRef = useRef<HTMLInputElement>(null);
-  const formRef = useRef<HTMLFormElement>(null);
 
   const current_user_friend_data = useMemo(() => {
     return friends_data?.find(friend => {
@@ -125,75 +127,38 @@ const ProfilePage: NextPage<ProfilePageProps> = ({ profile_data, friends_data })
         <form
           onSubmit={e => e.preventDefault()}
         >
+          <canvas id="canvas"></canvas>
           <input
             type="file"
             ref={imageUploadInputRef}
             accept="image/*"
             onChange={async e => {
               let file: any = e.currentTarget.files?.[0]
-              // sharp(buffer).toFile(`${session?.user_id}`, (e, i) => console.log({ e, i }))
-              const p = new Promise((resolve, reject) => {
-                const reader = new FileReader()
-                reader.onload = e => {
-                  resolve(e.target?.result)
-                }
-                reader.onerror = e => {
-                  reject(e)
-                }
-                reader.readAsText(file as any);
-              })
-              const buffer: any = await p;
-              console.log({ buffer })
-              const formData = new FormData()
-              console.log(file.data)
-              formData.append('file', file)
-              // formData.append(
-              //   "filename",
-              //   `${session?.user_id}-${file.name.replace(/[^\x00-\x7F]/g, "")}`
-              // );
-              // console.log({ buffer, bufferFrom: Buffer.from(buffer)})
-              // p.then(async (dataUrl: any) => {
-              const [data, error] = await resolver(axios.post(`/api/users/profile-data/update-profile-image?user_id=${session?.user_id}`, formData, { headers: {/*  'Content-Type': 'multipart/form-data' */}}))
-              console.log({ data, error })
-              // const body = new FormData();
-              // console.log(e.currentTarget.files)
-              // file = URL.createObjectURL(file);
-              //
-              // body.append('file', URL.createObjectURL(file as any), fileName: )
-              // const [data, error] = await resolver(axios.post(`/api/users/profile-data/update-profile-image?user_id=${session?.user_id}`, body, { headers: { 'Content-Type': 'multipart/form-data' } }))
-              // console.log({ data, error })
-              // const file = e.target.files[0];
-              // const reader = new FileReader()
-              // reader.onload = async event => {
-              //   const buffer = event.target?.result
-              //     headers: {
-              //       'Content-Type': 'application/octet-stream'
-              //     }
-              //   }))
-              //   console.log({ data, error })
-              //   const newCacheKey = await updateSession({ ...session, ...data })
-              //   console.log({ newCacheKey })
-              // }
-              // reader.onerror = error => console.error(error)
-              // reader.readAsBinaryString(file)
-              // console.log({ data: e.target })
-              // console.log({ file })
-              // const [data, error] = await resolver(axios.post('/api/users/profile-data/update-profile-image', file))
-              // console.log({ data, error })
-              // const [data, error] = await upload(
-              //   file,
-              //   "profile_images",
-              //   profile_data?.user_id
-              // );
-              // if (error || !data) {
-              //   console.log("whoops", { error });
-              //   return;
-              // }
-              // const cache_key = await updateSession({
-              //   ...session,
-              //   image_url: data.image_url,
-              // });
-              // window.location.reload()
+              const imageObjectUrl = URL.createObjectURL(file);
+              const canvas: any = document.getElementById('canvas')
+              const image = document.createElement('img')
+              const context = canvas?.getContext('2d')
+              image.onload = async () => {
+                canvas.width = image.naturalWidth
+                canvas.height = image.naturalHeight
+                context?.drawImage(image, 0, 0)
+                const newFileUrl = canvas.toDataURL('image/webp', 0.6)
+                const imageBlob = (() => {
+                  const byteString = atob(newFileUrl.split(',')[1])
+                  const mimeString = newFileUrl.split(',')[0].split(':')[1].split(';')[0]
+                  const ab = new ArrayBuffer(byteString.length)
+                  const ia = new Uint8Array(ab)
+                  for (let i = 0; i < byteString.length; i++) ia[i] = byteString.charCodeAt(i)
+
+                  return new Blob([ia], { type: mimeString })
+                })()
+                const newFile: any = new File([imageBlob], `${session?.user_id}.webp`, { type: imageBlob.type })
+                let [data, error] = await upload(newFile, 'profile_images', session?.user_id);
+                [data, error] = await resolver(axios.patch(`/api/users/profile-data/update-profile-image?user_id=${data.user_id}&image_url=${data.image_url}`))
+                updateSession({ ...session, image_url: data.image_url })
+                await revalidate(`/users/${profile_data.user_id}`)
+              }
+              image.src = imageObjectUrl
             }}
           />
         </form>
@@ -235,7 +200,7 @@ const ProfilePage: NextPage<ProfilePageProps> = ({ profile_data, friends_data })
               className={styles.profile_image}
               width={144}
               height={144}
-              src={profile_data.image_url}
+              src={profile_data.user_id === session?.user_id ? session?.image_url : profile_data.image_url}
             />
             <div>
               <h1 className={styles.display_name}>
@@ -264,28 +229,76 @@ const ProfilePage: NextPage<ProfilePageProps> = ({ profile_data, friends_data })
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const { user_id }: any = context.params;
-  let { data: profile_data } = await axios.get(
-    `${getApiUrl()}/api/users/profile-data/${user_id}`
-  );
-  let { data: friends_data } = await axios.get(
-    `${getApiUrl()}/api/friends/get-friends?user_id=${user_id}`
-  )
+  const prisma = new PrismaClient();
+  let friends_data = await prisma.friends.findMany({
+    select: {
+      friend_id: true,
+      sender: {
+        select: {
+          user_id: true,
+          display_name: true,
+          image_url: true,
+        }
+      },
+      recipient: {
+        select: {
+          user_id: true,
+          display_name: true,
+          image_url: true,
+        }
+      },
+      status: true,
+      created_at: true,
+      updated_at: true
+    },
+    where: {
+      OR: [
+        {
+          sender_user_id: user_id as string
+        },
+        {
+          recipient_user_id: user_id as string
+        }
+      ]
+    }
+  });
+  let profile_data = await prisma.users.findFirst({
+    select: {
+      user_id: true,
+      email: true,
+      updated_at: true,
+      created_at: true,
+      image_url: true,
+      display_name: true,
+      date_of_birth: true,
+      tagline: true,
+      sender: true,
+      recipient: true
+    },
+    where: { user_id: user_id as string },
+  });
+  friends_data = JSON.parse(JSON.stringify(friends_data))
+  profile_data = JSON.parse(JSON.stringify(profile_data))
+  prisma.$disconnect();
   return {
     props: { profile_data, friends_data },
-    revalidate: 300,
   };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { data: user_ids } = await axios.get(
-    `${getApiUrl()}/api/users/get-user-ids`
-  );
+  const prisma = new PrismaClient();
+  const user_ids = await prisma.users.findMany({
+    select: {
+      user_id: true,
+    },
+  });
   const paths = user_ids.map((x: any) => {
     return { params: { user_id: x.user_id } };
   });
+  prisma.$disconnect();
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
